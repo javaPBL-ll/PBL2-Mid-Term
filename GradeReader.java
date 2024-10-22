9d65a2cbd18177c6593b4c6d2ac7dadd1455323e
동글이 <kdongwoo97@gmail.com>
2024-10-22 19:05:06 +0900

    몇가지 개선사항 추가

--- GradeReader.java
+++ GradeReader.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * GradeReader 클래스는 파일로부터 학생들의 성적 데이터를 읽습니다
@@ -13,16 +15,20 @@ import java.util.Scanner;
  */
 public class GradeReader
 {
-    // 스태틱 메소드로 바꾸면서 필요가 없어짐
-    // public String filePath;
+    //스태틱 메소드로 바꾸면서 필요가 없어짐
+    private String filePath;
+    private int highScore;
+    private List highScoreStudent;
 
-    // /**
-     // * GradeReader 클래스의 객체 생성자
-     // */
-    // public GradeReader(String filePath)
-    // {
-        // this.filePath = filePath;
-    // }
+    /**
+     * GradeReader 클래스의 객체 생성자
+     */
+    public GradeReader(String filePath)
+    {
+        this.filePath = filePath;
+        this.highScore = 0;
+        this.highScoreStudent = new ArrayList<>();
+    }
 
     /**
      * 파일로부터 학생들의 이름과 성적을 HashMap에 저장합니다
@@ -30,38 +36,60 @@ public class GradeReader
      * @param 성적 데이터 파일을 경로 filePath
      * @return 학생의 이름을 key, 성적을 value로하는 HashMap을 반환합니다
      */
-    public static HashMap<String, Integer> readGrade(String filePath)
+    public HashMap<String, Integer> readGrade()
     {
         HashMap<String, Integer> studentGrade = new HashMap<>();
         try
         {
-            // FileReader fr = new FileReader(new File(filePath));
-            // Scanner sc = new Scanner(fr);
-            
-            // MS949 인코딩으로 파일을 읽기 위한 InputStreamReader와 Scanner 생성
             FileInputStream fr = new FileInputStream(new File(filePath));
             InputStreamReader isr = new InputStreamReader(fr, "MS949");
-            Scanner sc = new Scanner(isr);  // InputStreamReader를 이용하여 스캐너 생성
-            
+            Scanner sc = new Scanner(isr);
             while (sc.hasNext())
             {
                 String name = sc.next();
                 Integer score = sc.nextInt();
                 studentGrade.put(name, score);
+
+                if (score > highScore)
+                {
+                    highScore = score;
+                    highScoreStudent.clear();
+                    highScoreStudent.add(name);
+                }
+                else if(score == highScore)
+                {
+                    highScoreStudent.add(name);
+                }
             }
             fr.close();
             sc.close();
         }
-        
+
         catch (FileNotFoundException e)
         {
-            System.out.println("파일을 찾을 수 없습니다" + filePath);
+            System.out.println("파일을 찾을 수 없습니다");
+            System.out.println("파일경로가 올바른지 확인하세요");
+            System.out.println("입력된 파일 경로 = " + filePath);
+            System.exit(0);
         }
-        
+
         catch (Exception e)
         {
             System.out.println("오류가 발생했습니다");
+            System.out.println("입력파일에 문제가 있을 가능성이 있습니다");
+            System.exit(0);
         }
         return studentGrade;
+    }
+
+    /**
+     * 최고 점수를 가진 학생들
+     * 
+     * @param
+     * @return 최고 점수를 가진 학생들의 이름 리스트를 반환합니다
+     */
+    public List<String> getHighScoreStudent()
+    {
+        return this.highScoreStudent;
     }
 }
--- MyApp.java
+++ MyApp.java
@@ -1,4 +1,8 @@
 import java.util.HashMap;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 /**
  * GradeReader 클래스는 파일로부터 학생들의 성적 데이터를 읽습니다
@@ -9,26 +13,50 @@ import java.util.HashMap;
 public class MyApp {
     public static void main(String[] args)
     {
-        String filePath = "C:\\Temp\\inputData2024.txt";
+        String filePath = ":\\Temp\\inputData2024.txt";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("성적파일이 존재하는 드라이브문자를 입력하세요. 예)d");
+        String drivePath = sc.nextLine();
+        filePath = drivePath + filePath;
         
-        // 스태틱 메소드로 변환하기전 코드
-        // GradeReader gr = new GradeReader(filePath); 
-        // HashMap<String, Integer> sg = gr.readGrade();
-        
-        HashMap<String, Integer> sg = GradeReader.readGrade(filePath);
-        
+        GradeReader gr = new GradeReader(filePath); 
+        HashMap<String, Integer> sg = gr.readGrade();
+
         GradeCal gc = new GradeCal(sg);
         int totalScore = gc.calculateTotalScore();
         double averageScore = gc.calculateAverageScore();
 
-        
-        System.out.println("------ 학생 성적 리스트 ------");
-        for (String key : sg.keySet()) {
-            System.out.println(key + " : " + sg.get(key));
+        if(sg.size() == 0)
+        {
+            System.out.println("학생데이터가 존재하지 않습니다");
         }
-        
-        System.out.println("------ 총점 및 평균 ------");
-        System.out.println("총점: " + totalScore);
-        System.out.println("평균: " + averageScore);
+        else
+        {
+            System.out.println("------ 학생 성적 리스트 ------");
+            for(String key : sg.keySet())
+            {
+                System.out.println(key + " : " + sg.get(key));
+            }
+            
+            System.out.println("------ 학생 이름순 정렬 ------");
+            
+            List<String> name = new ArrayList<>(sg.keySet());
+            Collections.sort(name);
+            
+            for(String sn : name)
+            {
+                System.out.println(sn + " : " + sg.get(sn));
+            }
+            System.out.println("------ 계산 결과 ------");
+            System.out.println("총점: " + totalScore);
+            System.out.println("평균: " + averageScore);
+            
+            System.out.println("------ 최우수 학생 ------");
+            List<String> highScoreStudents = gr.getHighScoreStudent();
+            for (String hss : highScoreStudents)
+            {
+                System.out.println(hss + " : " + sg.get(hss));
+            }
+        }
     }
 }
